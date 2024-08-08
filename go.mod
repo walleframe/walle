45756805116818c10192eab41624828378bd5f15
czy <czy463@163.com>
2024-08-08 23:54:30 +0800

    add alert interface and update depend version

--- go.mod
+++ go.mod
@@ -6,12 +6,12 @@ require (
 	github.com/fsnotify/fsnotify v1.7.0
 	github.com/gogo/protobuf v1.3.2
 	github.com/golang/mock v1.6.0
-	github.com/google/uuid v1.5.0
+	github.com/google/uuid v1.6.0
 	github.com/json-iterator/go v1.1.12
 	github.com/spf13/pflag v1.0.5
-	github.com/stretchr/testify v1.8.4
+	github.com/stretchr/testify v1.9.0
 	go.uber.org/atomic v1.11.0
-	go.uber.org/zap v1.26.0
+	go.uber.org/zap v1.27.0
 )
 
 require (
@@ -20,6 +20,6 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ services/alert/alert.go
@@ -0,0 +1,99 @@
+package alert
+
+import (
+	"github.com/walleframe/walle/zaplog"
+	"go.uber.org/zap"
+)
+
+type AlertLevel int8
+
+const (
+	AlertLevelNone AlertLevel = iota
+	// AlertLevelWarning 警告信息 不是错误信息
+	AlertLevelWarning
+	// AlertLevelError 错误告警 需要处理，但不是很紧急
+	AlertLevelError
+	// AlertLevelCritical 关键信息告警
+	AlertLevelCritical
+	// AlertLevelEmergency 紧急告警 必须马上处理
+	AlertLevelEmergency
+)
+
+type AlertSupport interface {
+	Alert(lv AlertLevel, id int64, service, module, title, msg string, ext ...string)
+}
+
+var (
+	backend AlertSupport
+	service string
+)
+
+const (
+	defaultModule = "custom"
+)
+
+// SetupAlert 安装告警后端支持
+func SetupAlert(svc string, alert AlertSupport) {
+	backend = alert
+	service = svc
+}
+
+func Alert(lv AlertLevel, id int64, module, title, msg string, ext ...string) {
+	if backend == nil {
+		zaplog.GetFrameLogger().New("alert.Alert").Debug("alert not setup",
+			zap.Int8("lv", int8(lv)), zap.Int64("id", id), zap.String("service", service),
+			zap.String("module", module), zap.String("title", title), zap.String("msg", msg),
+			zap.Strings("ext", ext),
+		)
+		return
+	}
+	backend.Alert(lv, id, service, module, title, msg, ext...)
+}
+
+// Warning 警告信息 不是错误信息
+func Warning(title, msg string) {
+	if backend == nil {
+		zaplog.GetFrameLogger().New("alert.Warning").Debug("alert not setup",
+			zap.String("service", service),
+			zap.String("title", title), zap.String("msg", msg),
+		)
+		return
+	}
+	backend.Alert(AlertLevelWarning, 0, service, defaultModule, title, msg)
+}
+
+// Error 错误告警 需要处理，但不是很紧急
+func Error(title, msg string) {
+	if backend == nil {
+		zaplog.GetFrameLogger().New("alert.Error").Debug("alert not setup",
+			zap.String("service", service),
+			zap.String("title", title), zap.String("msg", msg),
+		)
+		return
+	}
+	backend.Alert(AlertLevelError, 0, service, defaultModule, title, msg)
+}
+
+// Critical 关键信息告警
+func Critical(title, msg string) {
+	if backend == nil {
+		zaplog.GetFrameLogger().New("alert.Critical").Debug("alert not setup",
+			zap.String("service", service),
+			zap.String("title", title), zap.String("msg", msg),
+		)
+		return
+	}
+	backend.Alert(AlertLevelCritical, 0, service, defaultModule, title, msg)
+}
+
+// Emergency 紧急告警 必须马上处理
+func Emergency(title, msg string) {
+	if backend == nil {
+		zaplog.GetFrameLogger().New("alert.Emergency").Debug("alert not setup",
+			zap.String("service", service),
+			zap.String("title", title), zap.String("msg", msg),
+		)
+		return
+	}
+	backend.Alert(AlertLevelEmergency, 0, service, defaultModule, title, msg)
+}
